ac362cc2864fcdd4ba237e87300a0a3f68e7c0a4
Maiorana Juan Diego <juanmaiorana23@gmail.com>
2023-05-30 00:14:50 -0300

    Practicas y refresco de html

--- app.js
+++ app.js
@@ -42,23 +42,23 @@
 // }
 
 // // -----------------SISTEMA DE TURNOS-------------------
-let cupos = 3;
-let cantTurnos = 0;
+// let cupos = 3;
+// let cantTurnos = 0;
 
-for (let turno = 1; turno < 10; turno++) {
-    let nombre = prompt("Ingrerse su nombre");
-    if (nombre == "Juan") {
-        alert("tomatela");
-        continue;
-    }
-    if (cupos < 1) {
-        alert("Lo sentimos, ya no hay turnos disponibles");
-        break;
-    }
-    alert(nombre + " tienes el turno N°" + turno);
-    cantTurnos = cantTurnos + 1;
-    cupos--;
-}
+// for (let turno = 1; turno < 10; turno++) {
+//     let nombre = prompt("Ingrerse su nombre");
+//     if (nombre == "Juan") {
+//         alert("tomatela");
+//         continue;
+//     }
+//     if (cupos < 1) {
+//         alert("Lo sentimos, ya no hay turnos disponibles");
+//         break;
+//     }
+//     alert(nombre + " tienes el turno N°" + turno);
+//     cantTurnos = cantTurnos + 1;
+//     cupos--;
+// }
 
 // // --------------WHILE----------
 
@@ -93,5 +93,177 @@ for (let turno = 1; turno < 10; turno++) {
 //                 comando = "Salir";
 //                 break;
 //             }
+//         default:
+//             alert("este comando no existe");
+//             break;
 //     }
 // }
+
+// ---------------------------CLASE 4(FUNCIONES)-----------------------------
+
+// function saludo(){
+//     alert("Hola");
+// }
+
+// saludo();
+
+function seguir() {
+    let continuar = parseInt(prompt("Desea realizar otra operacion? \n 1)Si \n 2)No"));
+    if (continuar == 1) {
+        calculadora();
+    } else if (continuar == 2) {
+        alert("Cerrando programa....");
+    } else {
+        alert("opcion invalida");
+        seguir();
+    }
+}
+
+function suma(val1, val2) {
+    let resultado = val1 + val2;
+    alert("La suma es:\n" + val1 + "+" + val2 + " = " + resultado);
+}
+
+function resta(val1, val2) {
+    let resultado = val1 - val2;
+    alert("La resta es:\n" + val1 + "-" + val2 + "=" + resultado);
+}
+
+function multiplicar(val1, val2) {
+    let resultado = val1 * val2;
+    alert("La multiplicacion es:\n" + val1 + "x" + val2 + "=" + resultado);
+}
+
+function dividir(val1, val2) {
+    let resultado = val1 / val2;
+    alert("La division es:\n" + val1 + "/" + val2 + "=" + resultado);
+}
+
+function calculadora() {
+    let val1 = parseInt(prompt("Ingrese el primer valor"));
+    let val2 = parseInt(prompt("Ingrese el segundo valor"));
+
+    comando = parseInt(prompt("Ingrese la operacion: \n 1) Suma\n 2) Resta \n 3)Multiplicacion \n 4)Division"));
+
+    switch (comando) {
+        case 1:
+            suma(val1, val2);
+            seguir();
+            break;
+        case 2:
+            resta(val1, val2);
+            seguir();
+            break;
+        case 3:
+            multiplicar(val1, val2);
+            seguir();
+            break;
+        case 4:
+            dividir(val1, val2);
+            seguir();
+            break;
+        default:
+            alert("ERROR");
+            break;
+    }
+}
+
+// --------------------------------------1RA PREENTREGA--------------------------------------
+
+function PreEntrega1() {
+
+    let comando2 = 0;
+    let carrito = 0;
+    let remeras = 0;
+    let pantalones = 0;
+    let accesorios = 0;
+
+    while (comando2 != "Salir") {
+
+        comando2 = parseInt(prompt("1) Productos\n 2) Metodos de pago \n 3)Carrito \n 4) Salir"));
+
+        switch (comando2) {
+
+            // PRODUCTOS
+            case 1:
+                let list2 = parseInt(prompt("Agregar producto al carrito: \n 1)Remeras \n 2)Pantalones \n 3)Accesorios"));
+
+                if (list2 == 1) {
+                    let eleccion = parseInt(prompt("Desea agregar 'Remeras' al carrito? \n 1)Si \n 2)No"));
+                    if (eleccion == 1) {
+                        alert("Producto agregado al carrito....");
+                        carrito = carrito + 1;
+                        remeras = remeras + 1;
+                        continue;
+                    } else if (eleccion == 2) {
+                        alert("Volviendo al principio....");
+                        continue;
+                    } else {
+                        alert("error");
+                        continue;
+                    }
+                } else if (list2 == 2) {
+                    let eleccion = parseInt(prompt("Desea agregar 'Pantalones' al carrito? \n 1)Si \n 2)No"));
+                    if (eleccion == 1) {
+                        alert("Producto agregado al carrito....");
+                        carrito = carrito + 1;
+                        pantalones = pantalones + 1;
+                        continue;
+                    } else if (eleccion == 2) {
+                        alert("Volviendo al principio....");
+                        continue;
+                    } else {
+                        alert("error");
+                        continue;
+                    }
+                } else if (list2 == 3) {
+                    let eleccion = parseInt(prompt("Desea agregar 'Accesoios' al carrito? \n 1)Si \n 2)No"));
+                    if (eleccion == 1) {
+                        alert("Producto agregado al carrito....");
+                        carrito = carrito + 1;
+                        accesorios = accesorios + 1;
+                        continue;
+                    } else if (eleccion == 2) {
+                        alert("Volviendo al principio....");
+                        continue;
+                    } else {
+                        alert("error");
+                        continue;
+                    }
+                }
+
+            // METODOS DE PAGO
+            case 2:
+                let metodos = prompt("Elija el metodo de pago:\n 1)Credito \n 2)Debito \n 3)Efectivo");
+                if (metodos == 1) {
+                    alert("Con tarjetas de credito tenes hasta 12 cuotas sin interes.")
+                } else if (metodos == 2) {
+                    alert("Con tarjetas de debito tenes hasta 3 cuotas en los productos de indumentaria.");
+                } else if (metodos == 3) {
+                    alert("Las compras en efectivo son de pago unico y en mano.");
+                } else {
+                    alert("ERROR");
+                }
+                break;
+
+            // CARRITO
+            case 3:
+                if (carrito == 0) {
+                    alert("Sin productos en el carrito");
+                } else {
+                    alert("Productos agregados al carrito: \n" + remeras + " Remeras \n" + pantalones + " Pantalones \n" + accesorios + " Accesorios");
+                }
+                break;
+
+            // SALIR
+            case 4:
+                comando2 = "Salir";
+                break;
+
+            default:
+                alert("este comando no existe");
+                break;
+
+        }
+    }
+}
